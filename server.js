0e78628d4c2eaeb6a04d3e3b7fcf5c9d1f075c45
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-02 11:24:26 -0500

    Updated welcome message in the root route.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Team Collaboration Tool!');
+  res.send('Welcome to the Collaborative Team Tool!');
 });
 
 // Socket.io setup
